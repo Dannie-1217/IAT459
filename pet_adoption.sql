22777d7e19438e8945bc9a188663853b55aa755f
Angus Wang <zekun_wang@sfu.ca>
2025-03-11 13:58:16 -0700

    Update pet_adoption.sql

--- pet_adoption.sql
+++ pet_adoption.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 11, 2025 at 09:34 PM
+-- Generation Time: Mar 11, 2025 at 09:56 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -36,6 +36,17 @@ CREATE TABLE `pet` (
   `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pet_tags`
+--
+
+CREATE TABLE `pet_tags` (
+  `pet_id` int(5) NOT NULL,
+  `tags` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -45,6 +56,22 @@ CREATE TABLE `pet` (
 --
 ALTER TABLE `pet`
   ADD PRIMARY KEY (`pet_id`);
+
+--
+-- Indexes for table `pet_tags`
+--
+ALTER TABLE `pet_tags`
+  ADD KEY `pet_id` (`pet_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pet_tags`
+--
+ALTER TABLE `pet_tags`
+  ADD CONSTRAINT `pet_tags_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `pet` (`pet_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
