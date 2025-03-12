2e70be3d78aed59d06437fc0a9bcc0a59ba84b3f
Angus Wang <zekun_wang@sfu.ca>
2025-03-11 22:26:26 -0700

    complete all tables

--- pet_adoption.sql
+++ pet_adoption.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 11, 2025 at 09:56 PM
+-- Generation Time: Mar 12, 2025 at 06:25 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `adoption_records`
+--
+
+CREATE TABLE `adoption_records` (
+  `user_id` int(5) NOT NULL,
+  `pet_id` int(5) NOT NULL,
+  `adopted_before` varchar(20) NOT NULL,
+  `other_pets` varchar(20) NOT NULL,
+  `suitable_living_space` varchar(20) NOT NULL,
+  `reason_for_adoption` text NOT NULL,
+  `adoption_date` date NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorites`
+--
+
+CREATE TABLE `favorites` (
+  `user_id` int(5) NOT NULL,
+  `pet_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pet`
 --
 
@@ -39,6 +67,17 @@ CREATE TABLE `pet` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pet_images`
+--
+
+CREATE TABLE `pet_images` (
+  `pet_id` int(5) NOT NULL,
+  `images` varbinary(20000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pet_tags`
 --
 
@@ -47,9 +86,50 @@ CREATE TABLE `pet_tags` (
   `tags` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `preferences`
+--
+
+CREATE TABLE `preferences` (
+  `user_id` int(5) NOT NULL,
+  `preferences_tags` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(5) NOT NULL,
+  `user_name` varchar(15) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `legal_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `other_contact` varchar(50) NOT NULL,
+  `profile_photo` varbinary(8000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `adoption_records`
+--
+ALTER TABLE `adoption_records`
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `pet_id` (`pet_id`);
+
+--
+-- Indexes for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `pet_id` (`pet_id`);
 
 --
 -- Indexes for table `pet`
@@ -58,20 +138,64 @@ ALTER TABLE `pet`
   ADD PRIMARY KEY (`pet_id`);
 
 --
+-- Indexes for table `pet_images`
+--
+ALTER TABLE `pet_images`
+  ADD KEY `pet_id` (`pet_id`);
+
+--
 -- Indexes for table `pet_tags`
 --
 ALTER TABLE `pet_tags`
   ADD KEY `pet_id` (`pet_id`);
 
 --
+-- Indexes for table `preferences`
+--
+ALTER TABLE `preferences`
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `adoption_records`
+--
+ALTER TABLE `adoption_records`
+  ADD CONSTRAINT `adoption_records_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `adoption_records_ibfk_2` FOREIGN KEY (`pet_id`) REFERENCES `pet` (`pet_id`);
+
+--
+-- Constraints for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`pet_id`) REFERENCES `pet` (`pet_id`);
+
+--
+-- Constraints for table `pet_images`
+--
+ALTER TABLE `pet_images`
+  ADD CONSTRAINT `pet_images_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `pet` (`pet_id`);
 
 --
 -- Constraints for table `pet_tags`
 --
 ALTER TABLE `pet_tags`
   ADD CONSTRAINT `pet_tags_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `pet` (`pet_id`);
+
+--
+-- Constraints for table `preferences`
+--
+ALTER TABLE `preferences`
+  ADD CONSTRAINT `preferences_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
