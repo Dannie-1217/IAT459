a2a649b92d8bfda96656a27970e49c51764b9661
Angus Wang <zekun_wang@sfu.ca>
2025-04-01 16:56:40 -0700

    Try to add pet tags

--- public/pages/pet_information.php
+++ public/pages/pet_information.php
@@ -48,5 +48,31 @@ else{
     echo"<tr>Result is empty!</tr>";
 }
 
+echo "Tags:";
+
+$tag_query = "SELECT tags.content FROM tags LEFT JOIN pet_tags ON pet_tags.tag_id = tags.tag_id WHERE pet_tags.pet_id = ".$id;
+
+$tag_result = mysqli_query($connection, $tag_query);
+
+if(!$tag_result){
+    echo"query faled!";
+    exit;
+}
+
+if(mysqli_num_rows($tag_result) != 0){
+    echo"<table class='resultTable'><tr>";          
+        echo "<td class='tableGrid'><p class='tableHeader'>Tags</p></td>";  
+    echo "</tr><tr>";
+    while($row = mysqli_fetch_assoc($select_result)){
+        echo "<td class='tableGrid'>". $row['content']. "</td>";  
+        echo"</tr>";
+    }
+    echo"</table>";
+}
+//If result is empty, then show this warning.
+else{
+    echo"<tr>Result is empty!</tr>";
+}
+
 require_once(ROOT_PATH . SHARED_PATH.'/footer.php');
 ?>
